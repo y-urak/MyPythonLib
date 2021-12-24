b436ebb290d53b8857272478f2b788bdf73a7b92
y-urak <yumau717@ezweb.ne.jp>
2021-12-24 18:03:52 +0900

    229

--- string_lib.py
+++ string_lib.py
@@ -10,11 +10,23 @@ def changeAsciiToChar(i,add=0):
 def zerofill(s="1234"):
     return s.zfill(8)
 
+##3 文字列の部分一致が簡単にできる
+def matchSubstring(s="abcdddxxxd"):
+    print('cdd' in s,'dxx' in s,'aci' in s)
+
+## 繰り返しの文字列
+def repeatMuchString(s="abc",n=3):
+    return s*3
+
+
 #テストコード
 
 if __name__ =='__main__':
     print(changeCharToAscii('a'),changeAsciiToChar(97))
     print(zerofill())
+    matchSubstring()
+    print(repeatMuchString())
 
-#(1) https://qiita.com/ell/items/6eb48e934a147898d823
-#(2) https://it-engineer-info.com/language/python/5664/
+#1 https://qiita.com/ell/items/6eb48e934a147898d823
+#2 https://it-engineer-info.com/language/python/5664/
+#3 https://note.nkmk.me/python-str-compare/#:~:text=source%3A%20str_compare.py-,%E9%83%A8%E5%88%86%E4%B8%80%E8%87%B4%3A%20in%2C%20not%20in,%E3%81%A8%20False%20%E3%81%8C%E8%BF%94%E3%81%95%E3%82%8C%E3%82%8B%E3%80%82
