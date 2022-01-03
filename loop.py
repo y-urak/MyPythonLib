b3c219a441ca4b2582ca5fa9e6c0c5fbc12080c0
y-urak <yumau717@ezweb.ne.jp>
2022-01-03 19:45:15 +0900

    1/3

--- loop.py
+++ loop.py
@@ -7,10 +7,29 @@ def inverseLoop(l):
     for ll in l[::-1]:
         print(ll)
 
-
+##2 loop内である値が見つかった時に終了->これ以降実行しない
+# 競プロ用
+def exitLoop(l):
+    for i in l:
+        if i == 3:
+            print("found")
+            exit()
+# returnの場合はその後の関数も実行してくれる
+def returnLoop(l):
+    for i in l:
+        if i == 3:
+            print("ffound")
+            return
 
 if __name__ == '__main__':
     inverseLoop([0,1,2,3,4,5])
+    print("")
+    returnLoop([1,2,3,4])
+    exitLoop([1,2,3,4,4,4])
+    returnLoop([1,2,3,4])
+
+
 
 
 #1 https://www.python.ambitious-engineer.com/archives/1757
+#2 https://www.sejuku.net/blog/24331
