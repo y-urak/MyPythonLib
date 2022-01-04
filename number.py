85e5d922b2766d19483a6d6c8e3952ccb6c5c97e
y-urak <yumau717@ezweb.ne.jp>
2022-01-04 15:22:12 +0900

    textbook

--- number.py
+++ number.py
@@ -1,3 +1,51 @@
+#素数判定
+def isPrime(n):
+    #どこまでみるか→Nの平方根
+    max=int(n**(1/2))
+    #1とnでしか割り切れない→素数
+    for i in range(2,max):
+        if n%i==0:
+            return False
+    return True
+#素数判定→応用例　約数checker
+def check_divisor(n):
+    # 素数チェックの応用
+    max=int(n**(1/2))
+    return_list=[]
+    for i in range(1,max):
+        if not(n%i==0): continue
+        # 割り切れたものを追加
+        return_list.append(i)
+        #ex 21=3*7の時に7の値も一緒に追加する
+        if not(i==int(n/i)):
+            return_list.append(int(n/i))
+
+    return return_list
+
+print(isPrime(100))
+print(check_divisor(100))
+
+#ユークリッドの互除法->最大公約数を求めるのに用いられる方法
+def GCD(a,b): #Greatest Common Divisor
+    c = max(a,b)
+    d = min(a,b)
+    while True:
+        if d==0:
+            return c
+        else:
+            c=c%d
+            tmp=d
+            d=c
+            c=tmp
+    #a==0 or b==0 ->max(a,b)
+    #if a>b: a=a%b
+    #else: b=b%a でもいいと思った
+
+print("GCD",GCD(24,40))
+#三つの場合のGCDの出し方
+print("GCD",GCD(GCD(24,40),60))
+
+
 #大きい値の作り方
 num1=1e9
 num2=(10**9)
--- sorting.py
+++ sorting.py
@@ -1,16 +1,38 @@
 import itertools
 #別のPCテスト
-##1 順列の例
+##1 順列の中身
 def permutation(list):
+    sum=0
+    #中身の出力
+    output_list=[]
     for p in itertools.permutations(list):
-        for a in p:
-            print(a)
-        print("next")
+        #中身の出力
+        print(p)
+        output_list.append(p)
+        #for a in p:
+        #    output_list.append(a)
+        sum+=1
+    print(sum)
+
+#1 順列の総数
+def permutation_num(n):
+    retval=1
+    for i in range(1,n+1):
+        retval*=i
+    return retval
+
     
 
 #range(n)->[0,.......,n-1]
 if __name__ =='__main__':
     permutation(range(4))
+    #4!
+    print(permutation_num(4))
+    #4P2
+    print(permutation_num(4)/permutation_num(2))
+    #4C2 ->n/r*n-r
+    print(permutation_num(4)/permutation_num(2)/permutation_num(2))
+
 
 #1 https://algo-logic.info/permutation/#toc_id_2_2
 # https://atcoder.jp/contests/abc232/editorial/3143
