8ec30a2d3781b1246da57ba62a022151793bb443
y-urak <yumau717@ezweb.ne.jp>
2022-01-27 18:12:32 +0900

    get image

---
+++ Selenium/get_csv_url_image.py
@@ -0,0 +1,25 @@
+'''
+CSVの中身を読み込んでURLから必要な画像を保存する
+D:\pythonProject\Web_Scraping\venv\Scripts\python.exe Selenium/screenshot_using_profile_module.py
+'''
+import csv
+import screenshot_using_profile_module as screenshot
+
+
+def csv_to_list(csv_name):
+    with open(csv_name,'r',encoding="utf-8_sig") as f:
+        reader = csv.reader(f)
+        list_csv = [row for row in reader]
+    return list_csv
+
+def get_kinokuniya_screenshot():
+    list_csv=csv_to_list('D:/pythonProject/pdfConverter/books.csv')
+    # NoとURLを取り出す -> 0 and 1
+    # 一番最初の行はタイトル行なので無視する
+    for i in range(1,len(list_csv)):
+        print(list_csv[i][0],list_csv[i][1])
+        screenshot.screenshot_kinokuniya(list_csv[i][1],str(int(list_csv[i][0])-1))
+
+
+if __name__ == "__main__":
+    get_kinokuniya_screenshot()
---
+++ Selenium/screenshot_using_profile_module.py
@@ -0,0 +1,79 @@
+'''
+各サイトに応じた画像のスクリーンショットを行う関数
+D:\pythonProject\Web_Scraping\venv\Scripts\python.exe Selenium/screenshot_using_profile_module.py
+'''
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def screenshot_kinokuniya(URL,file_name_output):
+    profile_path="C:/Users/xxbia/AppData/Local/Google/Chrome/User Data/Profile 2"
+    chrome_driver_path="D:/pythonProject/Web_Scraping/ver_97/chromedriver.exe"
+    #認証の情報を残すためにprofileを指定して開く 
+    #optionsにchromeのアカウントと紐づけた情報を渡している
+    options = webdriver.ChromeOptions()
+    options.add_argument("--user-data-dir=" + profile_path)
+    driver=webdriver.Chrome(executable_path=chrome_driver_path,options=options)
+
+    #urlを開く
+    driver.get(URL)
+
+    # ほしい情報を取り出す
+    # https://kurozumi.github.io/selenium-python/locating-elements.html
+    driver.implicitly_wait(30)
+    # XPATHで指定するのが楽　開発者ツールでコピペ可能
+    # https://www.octoparse.jp/blog/xpath-introduction/
+    img2 = driver.find_element(By.XPATH,'//*[@id="root"]/div[2]/div[1]/figure/img')
+    #スクリーンショットで記録
+    with open(f"C:/Users/xxbia/Downloads/"+file_name_output+".png", 'wb') as f:
+        f.write(img2.screenshot_as_png)
+
+    driver.quit()
+
+
+def screenshot_maruzen(URL,file_name_output):
+    profile_path="C:/Users/xxbia/AppData/Local/Google/Chrome/User Data/Profile 2"
+    chrome_driver_path="D:/pythonProject/Web_Scraping/ver_97/chromedriver.exe"
+    #認証の情報を残すためにprofileを指定して開く 
+    #optionsにchromeのアカウントと紐づけた情報を渡している
+    options = webdriver.ChromeOptions()
+    options.add_argument("--user-data-dir=" + profile_path)
+    driver=webdriver.Chrome(executable_path=chrome_driver_path,options=options)
+
+    #urlを開く
+    driver.get(URL)
+    driver.implicitly_wait(10)
+    test_app_dynamics_job(driver,URL)
+
+    driver.get(URL)
+    # ほしい情報を取り出す
+    # https://kurozumi.github.io/selenium-python/locating-elements.html
+    driver.implicitly_wait(30)
+    # 完全なXPATHだと上手くいく
+    img2 = driver.find_element(By.XPATH,'/html/body/div[1]/div[4]/div/a/img')
+    #スクリーンショットで記録
+    with open(f"C:/Users/xxbia/Downloads/"+file_name_output+".png", 'wb') as f:
+        f.write(img2.screenshot_as_png)
+
+    driver.quit()
+
+# seleniumの拡張機能でコード生成と調整
+yourID="v21e0000"
+yourPASS="passwords"
+def test_app_dynamics_job(driver,url="https://elib.maruzen.co.jp/elib/html/GuestLogin?1"):   
+    driver.get(url)
+    driver.find_element(By.ID,"id2c").click()
+    driver.find_element(By.ID,"username").click()
+    driver.find_element(By.ID,"username").clear()
+    driver.find_element(By.ID,"username").send_keys(yourID)
+    driver.find_element(By.ID,"password").clear()
+    driver.find_element(By.ID,"password").send_keys(yourPASS)
+    driver.implicitly_wait(5)
+    driver.find_element(By.NAME,"_eventId_proceed").click()
+    
+
+if __name__ == "__main__":
+    screenshot_kinokuniya("https://kinoden.kinokuniya.co.jp/oitauniv/bookdetail/p/KP00004921/","0")
+    #そもそも丸善はURLと画像が対応しているかも
+    screenshot_maruzen("https://elib.maruzen.co.jp/elib/html/BookDetail/Id/3000003265","11")
+    
